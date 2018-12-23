0f319e8d3e0a8567b51a5eb23bd0f22b412af5ae
maiko.kirino <sknmik3413@yahoo.co.jp>
2018-12-23 02:17:56 +0000

    user login #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def index
     @users = User.all.page(params[:page])
   end
@@ -13,17 +15,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-     if @user.save
-       flash[:success] = 'ユーザを登録しました。'
-       redirect_to @user
-      else
-        flash.now[:danger] = 'ユーザの登録に失敗しました。'
-        render :new
-      end
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
